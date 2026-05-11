7622663cced375665d37c483524349edd22ad646
PhilomenaNesci <philomena.nesci@gmail.com>
2026-05-11 10:46:49 -0400

    Add files via upload

---
+++ ch12/lab1202/rescue-bootstrap/scripts/script.js
@@ -0,0 +1,9 @@
+/*
+    Student Name: Philomena Nesci
+    File Name: script.js
+    Date: 5/8/2026
+*/
+//jQuery for hero image to consume the header window space
+	$(document) .ready(function() {
+	$('.hero').height($(window).height());
+	});
