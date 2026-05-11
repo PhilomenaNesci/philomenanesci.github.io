7007a382e045315282b62cc7e929cbfb46f822a8
PhilomenaNesci <philomena.nesci@gmail.com>
2026-05-11 13:49:59 -0400

    Add files via upload

---
+++ ch12/lab1203/student-bootstrap/scripts/script.js
@@ -0,0 +1,16 @@
+/*
+Group Member(s): Philomena Nesci
+File Name: script.js
+Date: 5/11/2026
+*/
+
+/* 
+Updated script.js for Bootstrap 
+*/
+
+function welcomeMessage() {
+    console.log("Welcome to the Official Website of the Augusta Curtis Concert Band!");
+}
+
+// Call the function
+welcomeMessage();
